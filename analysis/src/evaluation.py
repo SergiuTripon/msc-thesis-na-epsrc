0fb81b0373a49147ce86ad9630b2d0543d6f5a6e
SergiuTripon <contact@sergiu-tripon.com>
2016-08-20 00:34:13 +0100

    Added code for Comparison

--- analysis/src/evaluation.py
+++ analysis/src/evaluation.py
@@ -63,14 +63,67 @@ def calc_similarity(network, path):
         output_file.write('> {} similarity\n\n'.format(method.__name__.replace('similarity_', '').capitalize()))
 
         # write average similarity within communities to file
-        output_file.write('- Average similarity within communities:  {:.3f}\n'.format(average_within))
+        output_file.write('- Average within communities:  {:.3f}\n'.format(average_within))
 
         # print average similarity between communities to file
-        output_file.write('- Average similarity between communities: {:.3f}\n\n'.format(average_between))
+        output_file.write('- Average between communities: {:.3f}\n\n'.format(average_between))
 
         # print difference in similarity of edges within and between to file
-        output_file.write('- Difference in similarity between and within communities: '
+        output_file.write('- Difference between and within communities: '
                           '{:.3f}\n\n'.format(difference_within_between))
+
+
+########################################################################################################################
+
+
+# compares community structures
+def compare_community_structures(network, path):
+
+    # variable to hold louvain communities
+    louvain_c = network.community_multilevel(weights='weight')
+
+    # variable to hold spinglass communities
+    spinglass_c = network.community_spinglass(weights='weight')
+
+    ####################################################################################################################
+
+    # variable to hold comparison variation of information
+    comparison_vi = ig.compare_communities(louvain_c, spinglass_c, method='vi')
+
+    # variable to hold comparison normalized mutual information
+    comparison_nmi = ig.compare_communities(louvain_c, spinglass_c, method='nmi')
+
+    # variable to hold comparison split join
+    comparison_split_join = ig.compare_communities(louvain_c, spinglass_c, method='split-join')
+
+    # variable to hold comparison rand index
+    comparison_rand = ig.compare_communities(louvain_c, spinglass_c, method='rand')
+
+    # variable to hold comparison adjusted rand index
+    comparison_adjusted_rand = ig.compare_communities(louvain_c, spinglass_c, method='adjusted_rand')
+
+    ####################################################################################################################
+
+    # variable to hold output file
+    output_file = open('{}/network/txt/wnn/louvain/comparison.txt'.format(path), mode='a')
+
+    # write header to file
+    output_file.write('> Community structure comparison\n\n')
+
+    # write comparison using variation of information to file
+    output_file.write('- Variation of information:      {:.3f}\n'.format(comparison_vi))
+
+    # write comparison using normalized mutual information to file
+    output_file.write('- Normalized mutual information: {:.3f}\n'.format(comparison_nmi))
+
+    # write comparison using split join to file
+    output_file.write('- Split-join distance:           {}\n'.format(comparison_split_join))
+
+    # write comparison using rand index to file
+    output_file.write('- Rand index:                    {:.3f}\n'.format(comparison_rand))
+
+    # write comparison using adjusted rand index to file
+    output_file.write('- Adjusted Rand index:           {:.3f}\n'.format(comparison_adjusted_rand))
 
 
 ########################################################################################################################
@@ -87,9 +140,13 @@ def main():
 
     # calculate similarity
     calc_similarity(network, path)
-
     # print progress
     print('> Similarity calculated.')
+
+    # compare community structures
+    compare_community_structures(network, path)
+    # print progress
+    print('> Community structures compared.')
 
 
 ########################################################################################################################
